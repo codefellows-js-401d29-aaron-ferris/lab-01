8e6d4f2e44535a66842dfaa9fd41413960c338c1
abferris <abferris@gmail.com>
2019-03-19 11:28:47 -0700

    fixed all lint errors for travis

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -2,26 +2,26 @@
 
 //export
 
-const greet = require('../lib/greet.js')
+const greet = require('../lib/greet.js');
 
 describe('Greet Module', () => {
   //setting simple filepath
-  let greeting = greet.greeting
+  let greeting = greet.greeting;
 
   it('When input (name), returns Hello (name)', () => {
-    expect(greeting('Mr. Bean')).toEqual('Hello Mr. Bean')
+    expect(greeting('Mr. Bean')).toEqual('Hello Mr. Bean');
   });
 
   it('When input world, returns Hello world', () => {
-    expect(greeting('world')).toEqual('Hello world')
+    expect(greeting('world')).toEqual('Hello world');
   });
 
   it('When input is a number, returns null', () =>{
-    expect(greeting(5)).toEqual(null)
+    expect(greeting(5)).toEqual(null);
   });
 
   it('When input is a array, returns null', () =>{
-    expect(greeting(['Rowan Atkinson', 'Sean Bean'])).toEqual(null)
+    expect(greeting(['Rowan Atkinson', 'Sean Bean'])).toEqual(null);
   });
 
 });
